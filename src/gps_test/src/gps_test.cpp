de805bf574e1ca40e5b83a5ebb8388b58d2184da
Kose-i <tamura.kosei905@mail.kyutech.jp>
2018-06-03 11:53:21 +0900

    Modify src file

--- src/gps_test/src/gps_test.cpp
+++ src/gps_test/src/gps_test.cpp
@@ -3,9 +3,10 @@
 #include <sensor_msgs/NavSatFix.h>
 
 void callback(const sensor_msgs::NavSatFix& msgs){
-  ROS_INFO("%f, " , msgs.latitude);
-  ROS_INFO("%f, " , msgs.longitude);
-  ROS_INFO("%f, " , msgs.altitude);
+  /*ROS_INFO("緯度 = %f" , msgs.latitude);
+  ROS_INFO("経度 = %f" , msgs.longitude);
+  ROS_INFO("高度 = %f" , msgs.altitude);*/
+  ROS_INFO("latitude = %f, longtitude = %f, altitude = %f", msgs.latitude, msgs.longitude, msgs.altitude);
 }
 int main(int argc, char** argv){
   ros::init(argc, argv, "gps_server");
