0d7fe2e9897f0fc226c2bdebefd4e6bbfd1be1b8
Kose-i <tamura.kosei905@mail.kyutech.jp>
2018-06-14 21:53:17 +0900

    Remove bag

--- src/ros_command/src/test_getparam.cpp
+++ src/ros_command/src/test_getparam.cpp
@@ -1,11 +1,17 @@
 #include <ros/ros.h>
 
-int main(int argc, char** argc) {
-  ros::init(argc,argv,"rosparam_set");
+#include <thread>
+#include <chrono>
+
+int main(int argc, char** argv) {
+
+  ros::init(argc ,argv ,"rosparam_set");
   ros::NodeHandle nh;
 
   int set_test{};
   nh.getParam("/test/paramset", set_test);
+
+  std::this_thread::sleep_for(std::chrono::seconds(3));
   ROS_INFO("%d",set_test);
   return 0;
 }
