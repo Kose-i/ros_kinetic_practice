04fbf003bae0d66a2d4d723cb216b54424bdf70f
Kose-i <tamura.kosei905@mail.kyutech.jp>
2018-10-03 17:35:52 +0900

    modify learning_tf error

--- src/learning_tf/src/frame_tf_broadcaster.cpp
+++ src/learning_tf/src/frame_tf_broadcaster.cpp
@@ -11,10 +11,12 @@ int main(int argc, char** argv){
   ros::Rate rate(10.0);
   while (node.ok()){
     transform.setOrigin( tf::Vector3(2.0*sin(ros::Time::now().toSec()), 2.0*cos(ros::Time::now().toSec()), 0.0) );
-    transform.setRotation( tf::Quaternion(0, 0, 0, 1) );
+    //transform.setRotation( tf::Quaternion(0, 0, 0, 1) );
 
     br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "turtle1", "carrot1"));
+    ROS_INFO("check");
     rate.sleep();
   }
+  ROS_INFO("failure frame");
   return 0;
 };
--- src/learning_tf/src/turtle_tf_broadcaster.cpp
+++ src/learning_tf/src/turtle_tf_broadcaster.cpp
@@ -2,7 +2,7 @@
 #include <tf/transform_broadcaster.h>
 #include <turtlesim/Pose.h>
 
-static std::string turtle_name;
+std::string turtle_name;
 
 void poseCallback(const turtlesim::PoseConstPtr& msg){
   static tf::TransformBroadcaster br;
@@ -23,8 +23,7 @@ int main(int argc, char** argv){
   turtle_name = argv[1];
 
   ros::NodeHandle node;
-  ros::Subscriber sub = node.subscribe(turtle_name + "/pose",
-                                       10, &poseCallback);
+  ros::Subscriber sub = node.subscribe(turtle_name + "/pose", 10, &poseCallback);
 
   ros::spin();
   return 0;
